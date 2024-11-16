0749869f0579a8992f9fc8a86997642c26ee37f7
stazeng <36391796+stazeng@users.noreply.github.com>
2024-11-16 22:20:53 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -276,7 +276,7 @@ export default {
 
                     default:
                         return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.2bulu.com';
+                        url.hostname = 'www.speedtest.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
